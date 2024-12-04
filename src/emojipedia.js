4ea59bdd05b93cd6132334956cdec2497a285e57
nazanin <nazaninn.nell@gmail.com>
2024-12-04 22:10:11 +0330

    first commit

---
+++ src/emojipedia.js
@@ -0,0 +1,32 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "😑",
+    name: "Expressionless Face",
+    meaning:
+      "“I'm done with this” or “No words”. This emoji shows a neutral face with a straight mouth and no emotion, used to show frustration, boredom, or dislike."
+  },
+  {
+    id: 2,
+    emoji: "😍",
+    name: "Smiling Face with Heart-Eyes",
+    meaning:
+      "“This is amazing” or ”I'm in love!”. A smiling face with heart-shaped eyes, often used to show love, admiration, or excitement about someone or something amazing."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for rolling on the floor, laughing“."
+  },
+  {
+    id: 4,
+    emoji: "🤩",
+    name: "Star-Struck",
+    meaning:
+      "This is incredible! or I'm so impressed! A face with starry eyes, used to express amazement, excitement, or being impressed by something or someone extraordinary."
+  }
+];
+
+export default emojipedia;
